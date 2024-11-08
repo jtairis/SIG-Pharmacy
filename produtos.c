e4e718b17d16924ef3a82c104d7a9d7d9ca79603
arthur <103393571+THUR165@users.noreply.github.com>
2024-11-08 10:11:24 -0300

    adicionado validações atualizar produto

--- produtos.c
+++ produtos.c
@@ -126,25 +126,26 @@ void tela_pesquisar_produto(void) {
 }
 
 void tela_atualizar_produto(void) {
-    char codigo [8];
+    char codigo[8];
 
     system("clear||cls");
     printf("\n");
     printf("---------------------------------------------------------------------------\n");
-    printf("                                                                           \n");
     printf("          = = = = = Sistema de Gestão SIG-PHARMACY = = = = =               \n");
-    printf("                                                                           \n");
     printf("---------------------------------------------------------------------------\n");
-    printf("                                                                           \n");
     printf("                  = = = = = Atualizar Produto = = = = =                    \n");
-    printf("                                                                           \n");
-    getchar();
-    printf("          Digite o código para atualizar o produto: ");
-    scanf("%s 7[0-9]", codigo);
-    getchar();
-    printf("                                                                           \n");
     printf("---------------------------------------------------------------------------\n");
-    printf("\n");
+
+    do {
+        printf("          Digite o código para atualizar o produto: ");
+        scanf("%7s", codigo);
+        getchar();  // Limpa o buffer
+        if (!validar_codigo(codigo)) {
+            printf("Código inválido! Digite um código com 7 dígitos numéricos.\n");
+        }
+    } while (!validar_codigo(codigo));
+
+    printf("---------------------------------------------------------------------------\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...");
     getchar();
 }
