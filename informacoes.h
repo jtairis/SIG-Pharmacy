4dbc7a601b32f6e62fef41dde17534c6265a7d66
Leandro Sérgio <leandrooos222@gmail.com>
2025-01-22 16:44:17 -0300

    finalizacao das listas
    dei um clone no main para pegar as exatas alterações, e finalizei as listas

--- informacoes.h
+++
@@ -1,7 +0,0 @@
-//Aqui estão contidas as assinaturas das funções do arquivo informacoes.c, relacionadas ao Módulo Informações
-
-//Assinatura das funções
-void moduloinformacoes(void);
-int tela_menu_informacoes(void);
-void tela_equipe(void);
-void tela_sobre(void);
